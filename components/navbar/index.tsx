77db04f9f5fd3a0c985754ae0f9f8a219b823683
Tarik <tarik.lilic@stu.ibu.edu.ba>
2023-01-20 15:46:26 +0100

    form for components listing

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import ActiveLink from '../link'
 
 const navigation = [
     { name: 'Shop', href: '/', current: true },
@@ -43,17 +44,18 @@ export default function Navbar() {
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
-                                            <Link
+                                            <ActiveLink
                                                 key={item.name}
                                                 href={item.href}
-                                                className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                    'px-3 py-2 rounded-md text-sm font-medium'
-                                                )}
-                                                aria-current={item.current ? 'page' : undefined}
+                                                activeClass="bg-gray-900 text-white"
                                             >
-                                                {item.name}
-                                            </Link>
+                                                <div
+                                                    className={'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'}
+                                                    aria-current={item.current ? 'page' : undefined}
+                                                >
+                                                    {item.name}
+                                                </div>
+                                            </ActiveLink>
                                         ))}
                                     </div>
                                 </div>
