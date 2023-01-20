53b21975c3566ef09ef2113c75b92e78da7102b4
Tarik <tarik.lilic@stu.ibu.edu.ba>
2023-01-20 12:46:37 +0100

    navbar update

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Link from 'next/link'
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
+    { name: 'Shop', href: '/', current: true },
+    { name: 'Sell', href: '/pc/create', current: false }
 ]
 
 function classNames(...classes: string[]) {
@@ -44,7 +43,7 @@ export default function Navbar() {
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
-                                            <a
+                                            <Link
                                                 key={item.name}
                                                 href={item.href}
                                                 className={classNames(
@@ -54,7 +53,7 @@ export default function Navbar() {
                                                 aria-current={item.current ? 'page' : undefined}
                                             >
                                                 {item.name}
-                                            </a>
+                                            </Link>
                                         ))}
                                     </div>
                                 </div>
@@ -69,7 +68,7 @@ export default function Navbar() {
                                 </button>
 
                                 {/* Profile dropdown */}
-                                <Menu as="div" className="ml-3 relative">
+                                <Menu as="div" className="ml-3 relative z-10">
                                     <div>
                                         <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                             <span className="sr-only">Open user menu</span>
---
+++ pages/pc/create.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../../components";
+
+const PcCreate: NextPage = () => {
+    return (
+        <BaseLayout>
+            Component Create
+        </BaseLayout>
+    )
+}
+
+export default PcCreate;
